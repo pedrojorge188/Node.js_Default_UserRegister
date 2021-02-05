1e4e044b41473484c396a7063f6a28070e24fae3
pedrojorge188 <pedrojorge1882003@gmail.com>
2021-02-05 16:30:26 +0000

    cçe

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 
 dotenv.config({path: './.env'});
 
-const app = express();
+const app = express(); 
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
